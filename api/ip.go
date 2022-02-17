086889ea773a870de5d9c82727277c5274087dad
fnardn <56334451+niniubiwoc@users.noreply.github.com>
2022-02-17 21:01:13 +0800

    Create ip.go

---
+++ api/ip.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+    "fmt"
+    "net/http"
+    "io/ioutil"
+    "time"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+    resp, err := http.Get("http://myip.ipip.net/")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    fmt.Fprintf(w, string(body))
+}
